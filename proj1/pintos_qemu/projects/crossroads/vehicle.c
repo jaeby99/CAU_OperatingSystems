686ac4a2dfd7cb93ba3ece4a7cde630bb247e2c5
jaeby99 <qo0102@naver.com>
2022-12-26 08:55:07 +0900

    운영체제 과제

---
+++ proj1/pintos_qemu/projects/crossroads/vehicle.c
@@ -0,0 +1,129 @@
+
+#include <stdio.h>
+
+#include "threads/thread.h"
+#include "threads/synch.h"
+#include "projects/crossroads/vehicle.h"
+#include "projects/crossroads/map.h"
+#include "projects/crossroads/ats.h"
+
+/* path. A:0 B:1 C:2 D:3 */
+const struct position vehicle_path[4][4][10] = {
+	/* from A */ {
+		/* to A */
+		{{-1,-1},},
+		/* to B */
+		{{4,0},{4,1},{4,2},{5,2},{6,2},{-1,-1},},
+		/* to C */
+		{{4,0},{4,1},{4,2},{4,3},{4,4},{4,5},{4,6},{-1,-1},},
+		/* to D */
+		{{4,0},{4,1},{4,2},{4,3},{4,4},{3,4},{2,4},{1,4},{0,4},{-1,-1}}
+	},
+	/* from B */ {
+		/* to A */
+		{{6,4},{5,4},{4,4},{3,4},{2,4},{2,3},{2,2},{2,1},{2,0},{-1,-1}},
+		/* to B */
+		{{-1,-1},},
+		/* to C */
+		{{6,4},{5,4},{4,4},{4,5},{4,6},{-1,-1},},
+		/* to D */
+		{{6,4},{5,4},{4,4},{3,4},{2,4},{1,4},{0,4},{-1,-1},}
+	},
+	/* from C */ {
+		/* to A */
+		{{2,6},{2,5},{2,4},{2,3},{2,2},{2,1},{2,0},{-1,-1},},
+		/* to B */
+		{{2,6},{2,5},{2,4},{2,3},{2,2},{3,2},{4,2},{5,2},{6,2},{-1,-1}},
+		/* to C */
+		{{-1,-1},},
+		/* to D */
+		{{2,6},{2,5},{2,4},{1,4},{0,4},{-1,-1},}
+	},
+	/* from D */ {
+		/* to A */
+		{{0,2},{1,2},{2,2},{2,1},{2,0},{-1,-1},},
+		/* to B */
+		{{0,2},{1,2},{2,2},{3,2},{4,2},{5,2},{6,2},{-1,-1},},
+		/* to C */
+		{{0,2},{1,2},{2,2},{3,2},{4,2},{4,3},{4,4},{4,5},{4,6},{-1,-1}},
+		/* to D */
+		{{-1,-1},}
+	}
+};
+
+static int is_position_outside(struct position pos)			// 차량이 도로위를 벗어난 상태
+{
+	return (pos.row == -1 || pos.col == -1);
+}
+
+/* return 0:termination, 1:success, -1:fail */
+static int try_move(int start, int dest, int step, struct vehicle_info *vi)
+{
+	struct position pos_cur, pos_next;
+
+	pos_next = vehicle_path[start][dest][step];
+	pos_cur = vi->position;
+
+	if (vi->state == VEHICLE_STATUS_RUNNING) {
+		/* check termination */
+		if (is_position_outside(pos_next)) {
+			/* actual move */
+			vi->position.row = vi->position.col = -1;
+			/* release previous */
+			lock_release(&vi->map_locks[pos_cur.row][pos_cur.col]);			// 차량이 exit하기 전 위치의 lock을 release
+			return 0;
+		}
+	}
+
+	/* lock next position */
+	lock_acquire(&vi->map_locks[pos_next.row][pos_next.col]);				// 다음 이동 경로에 lock을 설정
+	if (vi->state == VEHICLE_STATUS_READY) {								// 이동할 준비가 된 차량이면 상태를 running으로 업데이트
+		/* start this vehicle */
+		vi->state = VEHICLE_STATUS_RUNNING;
+	} else {																// 이미 running이면 현재 위치의 lock을 release한다.
+		/* release current position */
+		lock_release(&vi->map_locks[pos_cur.row][pos_cur.col]);			
+	}
+	/* update position */
+	vi->position = pos_next;												// 차량이 다음 위치로 이동
+	
+	return 1;
+}
+
+void init_on_mainthread(int thread_cnt){
+	/* Called once before spawning threads */
+}
+
+void vehicle_loop(void *_vi)
+{
+	int res;
+	int start, dest, step;
+
+	struct vehicle_info *vi = _vi;
+
+	start = vi->start - 'A';						// vehicle_path[start][dest][]
+	dest = vi->dest - 'A';
+
+	vi->position.row = vi->position.col = -1;		// 초기화 상태 (-1,-1)
+	vi->state = VEHICLE_STATUS_READY;
+
+	step = 0;
+	while (1) {
+		/* vehicle main code */
+		res = try_move(start, dest, step, vi);
+		if (res == 1) {								// 차량 운행중	
+			step++;
+		}
+
+		/* termination condition. */ 
+		if (res == 0) {								// 차량이 도로에서 벗어남
+			break;
+		}
+
+		/* unitstep change! */
+		unitstep_changed();							// step값이 변할때마다 호출
+	}	
+
+	/* status transition must happen before sema_up */
+	vi->state = VEHICLE_STATUS_FINISHED;
+}
